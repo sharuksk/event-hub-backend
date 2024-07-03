e8ef2e0b747d2881fc552006d8087dcf278cfd27
unknown <aravindkumar40m@gmail.com>
2024-07-03 17:06:08 +0530

    signin & signup catchAsync and trycatch added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,55 @@
 // controllers/userController.js
 
 const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 //create a user
 
-exports.createUser = async (req, res) => {
+exports.createUser = catchAsync(async (req, res, next) => {
   try {
     const { email } = req.body;
 
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      res.status(400).json({ message: "user already exists" });
+      return next(new AppError("user already exists"));
     } else {
       const user = new User(req.body);
       await user.save();
-      res.status(200).json(user);
+      res.status(200).json({
+        status: "success",
+        data: {
+          user,
+        },
+      });
     }
   } catch (err) {
     res.status(400).json({ message: "user not found" });
   }
-};
+});
 
 // login
 
-exports.loginUser = async (req, res) => {
-  const { email, passwordHash } = req.body;
+exports.loginUser = catchAsync(async (req, res, next) => {
+  try {
+    const { email, passwordHash } = req.body;
 
-  const user = await User.findOne({ email });
-  console.log(user);
+    const user = await User.findOne({ email });
+    console.log(user);
 
-  if (user && (await user.matchPassword(passwordHash))) {
-    res.status(200).json({
-      email: email,
-      password: passwordHash,
-    });
-  } else {
-    res.status(400).json({ message: "Invalid email or password" });
+    if (user && (await user.matchPassword(passwordHash))) {
+      res.status(200).json({
+        status: "success",
+        data: {
+          email: email,
+          password: passwordHash,
+        },
+      });
+    } else {
+      return next(new AppError("Invalid email or password"));
+    }
+  } catch (err) {
+    res.status(400).json({ message: err });
   }
-};
+});
