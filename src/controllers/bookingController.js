96bf0f365f8dc2d6d4b993ba820adccb86881495
subin4693 <46subin@gmail.com>
2024-07-15 16:47:39 +0530

    save booking api changed

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -56,8 +56,11 @@ exports.createBooking = catchAsync(async (req, res, next) => {
   const uniqueId = uuidv4();
 
   itemIds.map(async (itemId) => {
+    const clientId = await Item.findById(itemId).select("clientId");
+    console.log(clientId);
     const newBooking = new Booking({
       ...req.body,
+      clientId: clientId.clientId,
       itemId,
       groupId: uniqueId,
     });
